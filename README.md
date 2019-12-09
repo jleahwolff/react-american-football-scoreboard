ea20e3995f3ee6bb5771aa63c099a8b232719fc6
Jessica Wolff <jleahwolff@gmail.com>
2019-12-09 17:00:43 -0600

    Done.

--- README.md
+++ README.md
@@ -23,8 +23,8 @@ In this project, you build an app that diplays a scoreboard for an american foot
 - [x] Download project dependencies by running one of these two commands `yarn` or `npm install`
 - [x] Using the same command tool (yarn or npm) start up the app using `yarn start` or `npm start`
 - [x] Create a new branch: git checkout -b `<firstName-lastName>`.
-- [ ] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
-- [ ] Push commits: git push origin `<firstName-lastName>`.
+- [x] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
+- [x] Push commits: git push origin `<firstName-lastName>`.
 
 Follow these steps for completing your project.
 
@@ -32,7 +32,6 @@ Follow these steps for completing your project.
 - [ ] Add your team lead as a reviewer on the pull-request
 - [ ] Your team lead will count the project as complete by merging the branch back into master.
 - [ ] Do your magic!
-
 ## Minimum Viable Product
 
 1. Hold each team's current score in a state value
--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homePoints, setHomePoints] = useState(0);
+  const [awayPoints, setAwayPoints] = useState(0);
+
+  const homeScoredTD = e => {
+    setHomePoints(homePoints + 6);
+  }
+
+  const homeScoredFG = e => {
+    setHomePoints(homePoints +2);
+  }
+
+  const awayScoredTD = e => {
+    setAwayPoints(awayPoints + 6);
+  }
+
+  const awayScoredFG = e => {
+    setAwayPoints(awayPoints + 2);
+  }
 
   return (
     <div className="container">
@@ -15,12 +33,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homePoints}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayPoints}</div>
           </div>
         </div>
         <BottomRow />
@@ -28,12 +46,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick = {homeScoredTD}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick = {homeScoredFG}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick = {awayScoredTD}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick = {awayScoredFG}>Away Field Goal</button>
         </div>
       </section>
     </div>
